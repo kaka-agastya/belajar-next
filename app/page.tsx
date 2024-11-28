3ea6a9d598cec38d57dae205d8826652806f22a7
Kaka <kakagastya2@gmail.com>
2024-11-28 09:38:01 +0700

    change

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  const nama = "Kaka Agastya"
+  const nama = "Kaka"
   return (
     <div className ="text-red-500">
       {nama}
