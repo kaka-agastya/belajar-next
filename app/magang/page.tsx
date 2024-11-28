0122ba6f3ede3d2590cec86d1509833fc4609ac7
Kaka <kakagastya2@gmail.com>
2024-11-28 09:21:09 +0700

    add app

---
+++ app/magang/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Magang() {
+  const magang = "PGR"
+  return (
+    <div className ="italic font-bold">
+      {magang}
+    </div>
+
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  const nama = "Kaka Agastya"
+  return (
+    <div className ="text-red-500">
+      {nama}
+    </div>
+
+  );
+}
