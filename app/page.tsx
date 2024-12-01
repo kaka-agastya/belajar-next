8ec2b7fcfe6846afd3c03a181f1bfd5c781f0e36
Kaka <kakagastya2@gmail.com>
2024-12-01 19:57:20 +0700

    heunm

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
 import Image from "next/image";
-import FadeIn from '../components/ui/FadeIn';
 
 export default function Home() {
   const nama = "Kaka Agastya"
   return (
     <section className="text-center py-20">
-      <FadeIn>
+    
       <h1 className="text-4xl font-bold">Welcome to My Portfolio</h1>
-      </FadeIn>
-      <FadeIn>
+      
+    
       <p className="text-gray-600 mt-4">
         I'm a web developer passionate about building modern web applications.
       </p>
-      </FadeIn>
+      
     </section>
   );
 }
